b34461f8dab87a702d0381e7564da88905210ea7
minus3theta <yone_j_synthesis@yahoo.co.jp>
2016-10-02 21:28:23 +0900

    Initial commit (templates)

---
+++ atcoder/template.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <list>
+#include <map>
+#include <queue>
+#include <stack>
+#include <set>
+#include <vector>
+
+#define REP(i,s,n) for(int i=(int)(s);i<(int)(n);i++)
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+  return 0;
+}
