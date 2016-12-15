02b9dc3310f94455d61fc610af35c70f36774fc7
minus3theta <yone_j_synthesis@yahoo.co.jp>
2016-12-15 17:47:29 +0900

    Add lib/segtree.cpp

---
+++ lib/segtree.cpp
@@ -0,0 +1,42 @@
+template <class T, class Op = T (*) (T,T)>
+struct Segtree {
+  int n;
+  vector<T> dat;
+  Op op;
+  T unit;
+  segtree(int al, Op op, T unit) : op(op), unit(unit) {
+    n = 1;
+    while(n < al) {
+      n *= 2;
+    }
+    dat = vector<T>(2 * n - 1, unit);
+  }
+  segtree(const vector<T> &arr, Op op, T unit) : op(op), unit(unit) {
+    int al = arr.size();
+    n = 1;
+    while(n < al) {
+      n *= 2;
+    }
+    dat = vector<T>(2 * n - 1, unit);
+    copy(arr.begin(), arr.end(), dat.begin() + n - 1);
+    for(int i=n-2; i >= 0; i--) {
+      dat[i] = op(dat[i * 2 + 1], dat[i * 2 + 2]);
+    }
+  }
+  void update(int k, T a) {
+    k += n - 1;
+    dat[k] = a;
+    while(k > 0) {
+      k = (k - 1) / 2;
+      dat[k] = op(dat[k * 2 + 1], dat[k * 2 + 2]);
+    }
+  }
+  T query(int a, int b, int k = 0, int l = 0, int r = -1) {
+    if(r < 0) r = n;
+    if(r <= a || b <= l) return unit;
+    if(a <= l && r <= b) return dat[k];
+    T vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
+    T vr = query(a, b, k * 2 + 2, (l + r) / 2, r);
+    return op(vl, vr);
+  }
+};
