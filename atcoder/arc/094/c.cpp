df329cf80b94e76f1a26b8879e3734c66b1017c1
minus3theta <yone_j_synthesis@yahoo.co.jp>
2018-04-09 23:48:41 +0900

    Add ARC 094 C

---
+++ atcoder/arc/094/c.cpp
@@ -0,0 +1,50 @@
+#include <cstdio>
+#include <climits>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <tuple>
+#include <list>
+#include <map>
+#include <queue>
+#include <stack>
+#include <set>
+#include <vector>
+#include <iterator>
+#include <regex>
+
+#define REP(i,s,n) for(int i=(int)(s);i<(int)(n);i++)
+#define FOR(x,xs) for(auto &x: xs)
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> PI;
+typedef pair<ll,ll> PL;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+
+template <class T, template <class, class> class C, class charT = char>
+void vdump(const C<T, allocator<T>> &v, const charT* delimiter = ", ",
+           ostream &stream = cout) {
+  copy(v.begin(), v.end(), ostream_iterator<T>(stream, delimiter));
+  stream << endl;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  int a, b, c;
+  cin >> a >> b >> c;
+  int m = max(a, max(b, c));
+  int sum = a + b + c;
+  if(sum % 2 == m % 2) {
+    cout << (m * 3 - sum) / 2 << endl;
+  } else {
+    cout << ((m+1) * 3 - sum) / 2 << endl;
+  }
+
+  return 0;
+}
