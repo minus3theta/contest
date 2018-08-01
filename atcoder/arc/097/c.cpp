845b13a462e99b434c41120de47b70bef4dea0a5
minus3theta <yone_j_synthesis@yahoo.co.jp>
2018-08-02 00:29:16 +0900

    Add ARC 097 C

---
+++ atcoder/arc/097/c.cpp
@@ -0,0 +1,55 @@
+#include <cstdio>
+#include <climits>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <tuple>
+#include <list>
+#include <map>
+#include <queue>
+#include <stack>
+#include <set>
+#include <vector>
+#include <iterator>
+#include <regex>
+
+#define REP(i,s,n) for(int i=(int)(s);i<(int)(n);i++)
+#define FOR(x,xs) for(auto &x: xs)
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> PI;
+typedef pair<ll,ll> PL;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+
+template <class T, template <class, class> class C, class charT = char>
+void vdump(const C<T, allocator<T>> &v, const charT* delimiter = ", ",
+           ostream &stream = cout) {
+  copy(v.begin(), v.end(), ostream_iterator<T>(stream, delimiter));
+  stream << endl;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  string s;
+  int k;
+  cin >> s;
+  cin >> k;
+  vector<string> ss;
+  int n = s.size();
+  REP(i,0,n) {
+    REP(j,1,k+1) {
+      ss.push_back(s.substr(i,j));
+    }
+  }
+  sort(ss.begin(), ss.end());
+  unique(ss.begin(), ss.end());
+  cout << ss[k-1] << endl;
+
+  return 0;
+}
