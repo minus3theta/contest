d040fae8f76c5048017f362156d4527564e8123b
minus3theta <yone_j_synthesis@yahoo.co.jp>
2019-05-27 19:33:47 +0900

    Add Code Festival 2016 Qual B C

---
+++ atcoder/others/fes2016b/c.rs
@@ -0,0 +1,78 @@
+#[allow(unused_imports)]
+use std::cmp;
+
+// Input macro from https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+  (source = $s:expr, $($r:tt)*) => {
+    let mut iter = $s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+  ($($r:tt)*) => {
+    #[allow(unused_mut)]
+    let mut s = {
+      use std::io::Read;
+      let mut s = String::new();
+      std::io::stdin().read_to_string(&mut s).unwrap();
+      s
+    };
+    let mut iter = s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+}
+
+macro_rules! input_inner {
+  ($iter:expr) => {};
+  ($iter:expr, ) => {};
+
+  ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+    let $var = read_value!($iter, $t);
+    input_inner!{$iter $($r)*}
+  };
+}
+
+macro_rules! read_value {
+  ($iter:expr, ( $($t:tt),* )) => {
+    ( $(read_value!($iter, $t)),* )
+  };
+
+  ($iter:expr, [ $t:tt ; $len:expr ]) => {
+    (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+  };
+
+  ($iter:expr, chars) => {
+    read_value!($iter, String).chars().collect::<Vec<char>>()
+  };
+
+  ($iter:expr, usize1) => {
+    read_value!($iter, usize) - 1
+  };
+
+  ($iter:expr, $t:ty) => {
+    $iter.next().unwrap().parse::<$t>().expect("Parse error")
+  };
+}
+
+fn main() {
+  input! {
+    w: usize,
+    h: usize,
+    ps: [i64; w],
+    qs: [i64; h],
+  }
+  let mut edges: Vec<_> = ps.into_iter().map(|p| (p, true)).chain(qs.into_iter().map(|q| (q, false))).collect();
+  edges.sort();
+  let mut total = 0;
+  let mut we = w as i64 + 1;
+  let mut he = h as i64 + 1;
+  for &(c, horizontal) in edges.iter() {
+    if horizontal {
+      total += he * c;
+      we -= 1;
+    } else {
+      total += we * c;
+      he -= 1;
+    }
+  }
+
+  println!("{}", total);
+}
