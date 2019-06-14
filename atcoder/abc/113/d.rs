ffa48b97a490ea0cbb6a207905e4b2ee1feb17e4
minus3theta <yone_j_synthesis@yahoo.co.jp>
2019-06-14 14:04:42 +0900

    Add ABC 113 D

---
+++ atcoder/abc/113/d.rs
@@ -0,0 +1,187 @@
+#[allow(unused_imports)]
+use std::cmp;
+
+// Input macro from https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+  (source = $s:expr, $($r:tt)*) => {
+    let mut iter = $s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+  ($($r:tt)*) => {
+    #[allow(unused_mut)]
+    let mut s = {
+      use std::io::Read;
+      let mut s = String::new();
+      std::io::stdin().read_to_string(&mut s).unwrap();
+      s
+    };
+    let mut iter = s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+}
+
+macro_rules! input_inner {
+  ($iter:expr) => {};
+  ($iter:expr, ) => {};
+
+  ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+    let $var = read_value!($iter, $t);
+    input_inner!{$iter $($r)*}
+  };
+}
+
+macro_rules! read_value {
+  ($iter:expr, ( $($t:tt),* )) => {
+    ( $(read_value!($iter, $t)),* )
+  };
+
+  ($iter:expr, [ $t:tt ; $len:expr ]) => {
+    (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+  };
+
+  ($iter:expr, chars) => {
+    read_value!($iter, String).chars().collect::<Vec<char>>()
+  };
+
+  ($iter:expr, usize1) => {
+    read_value!($iter, usize) - 1
+  };
+
+  ($iter:expr, $t:ty) => {
+    $iter.next().unwrap().parse::<$t>().expect("Parse error")
+  };
+}
+
+const MOD: i64 = 1_000_000_007;
+
+#[derive(Copy, Clone, Hash, Debug)]
+struct ModInt {
+  x: i64
+}
+impl ModInt {
+  fn new(x: i64) -> Self {
+    ModInt {
+      x: x % MOD
+    }
+  }
+  fn pow(self, e: i64) -> Self {
+    if e == 0 {
+      Self::new(1)
+    } else if e % 2 == 1 {
+      self.pow(e - 1) * self
+    } else {
+      let y = self.pow(e / 2);
+      y * y
+    }
+  }
+  #[allow(dead_code)]
+  fn inv(self) -> Self {
+    self.pow(MOD - 2)
+  }
+}
+impl<T: Into<ModInt>> std::ops::Add<T> for ModInt {
+  type Output = Self;
+  fn add(self, other: T) -> Self {
+    let other = other.into();
+    let sum = self.x + other.x;
+    ModInt {
+      x: if sum >= MOD { sum - MOD } else { sum }
+    }
+  }
+}
+impl<T: Into<ModInt>> std::ops::Sub<T> for ModInt {
+  type Output = Self;
+  fn sub(self, other: T) -> Self {
+    let other = other.into();
+    let sum = self.x - other.x;
+    ModInt {
+      x: if sum < 0 { sum + MOD } else { sum }
+    }
+  }
+}
+impl<T: Into<ModInt>> std::ops::Mul<T> for ModInt {
+  type Output = Self;
+  fn mul(self, other: T) -> Self {
+    let other = other.into();
+    ModInt {
+      x: self.x * other.x % MOD
+    }
+  }
+}
+impl<T: Into<ModInt>> std::ops::Div<T> for ModInt {
+  type Output = Self;
+  fn div(self, other: T) -> Self {
+    let other = other.into();
+    self * other.inv()
+  }
+}
+impl From<i64> for ModInt {
+  fn from(x: i64) -> Self {
+    ModInt::new(x)
+  }
+}
+impl std::fmt::Display for ModInt {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    self.x.fmt(f)
+  }
+}
+impl<T: Into<ModInt>> std::ops::AddAssign<T> for ModInt {
+  fn add_assign(&mut self, other: T) {
+    *self = *self + other;
+  }
+}
+impl<T: Into<ModInt>> std::ops::SubAssign<T> for ModInt {
+  fn sub_assign(&mut self, other: T) {
+    *self = *self - other;
+  }
+}
+impl<T: Into<ModInt>> std::ops::MulAssign<T> for ModInt {
+  fn mul_assign(&mut self, other: T) {
+    *self = *self * other;
+  }
+}
+impl<T: Into<ModInt>> std::ops::DivAssign<T> for ModInt {
+  fn div_assign(&mut self, other: T) {
+    *self = *self / other;
+  }
+}
+
+fn valid_row(mut x: u32) -> bool {
+  while x > 0 {
+    if x & 1 != 0 && x & 2 != 0 {
+      return false;
+    }
+    x >>= 1;
+  }
+  true
+}
+
+fn main() {
+  input! {
+    h: usize,
+    w: usize,
+    k: usize1,
+  }
+  let mut dp = vec![vec![ModInt::new(0); w]; h + 1];
+  dp[0][0] = 1.into();
+  for r in 0 .. h {
+    for lines in 0 .. 1 << (w - 1) {
+      if ! valid_row(lines) {
+        continue;
+      }
+      if lines & 1 == 0 {
+        dp[r+1][0] += dp[r][0];
+      }
+      for c in 0 .. w - 1 {
+        if (lines >> c) & 1 != 0 {
+          dp[r+1][c] += dp[r][c+1];
+          dp[r+1][c+1] += dp[r][c];
+        } else if (lines >> (c + 1)) & 1 == 0 {
+          dp[r+1][c+1] += dp[r][c+1];
+        }
+      }
+    }
+  }
+
+  println!("{}", dp[h][k]);
+}
