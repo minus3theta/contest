f3da0702b2b8f6d3aaaacacba8efc0e081770c9b
minus3theta <yone_j_synthesis@yahoo.co.jp>
2019-06-27 19:04:11 +0900

    Add AGC 020 B

---
+++ atcoder/agc/020/b.rs
@@ -0,0 +1,78 @@
+#[allow(unused_imports)]
+use std::cmp;
+
+// Input macro from https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+  (source = $s:expr, $($r:tt)*) => {
+    let mut iter = $s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+  ($($r:tt)*) => {
+    let s = {
+      use std::io::Read;
+      let mut s = String::new();
+      std::io::stdin().read_to_string(&mut s).unwrap();
+      s
+    };
+    let mut iter = s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+}
+
+macro_rules! input_inner {
+  ($iter:expr) => {};
+  ($iter:expr, ) => {};
+
+  ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+    let $var = read_value!($iter, $t);
+    input_inner!{$iter $($r)*}
+  };
+}
+
+macro_rules! read_value {
+  ($iter:expr, ( $($t:tt),* )) => {
+    ( $(read_value!($iter, $t)),* )
+  };
+
+  ($iter:expr, [ $t:tt ; $len:expr ]) => {
+    (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+  };
+
+  ($iter:expr, chars) => {
+    read_value!($iter, String).chars().collect::<Vec<char>>()
+  };
+
+  ($iter:expr, usize1) => {
+    read_value!($iter, usize) - 1
+  };
+
+  ($iter:expr, $t:ty) => {
+    $iter.next().unwrap().parse::<$t>().expect("Parse error")
+  };
+}
+
+fn solve(aa: &Vec<i64>) -> Option<(i64, i64)> {
+  let mut l = 2;
+  let mut r = 2;
+  for &a in aa.iter().rev() {
+    let next_l = (l + a - 1) / a * a;
+    let next_r = r / a * a + a - 1;
+    if next_l > next_r {
+      return None;
+    }
+    l = next_l;
+    r = next_r;
+  }
+  Some((l, r))
+}
+
+fn main() {
+  input! {
+    k: usize,
+    aa: [i64; k],
+  }
+  match solve(&aa) {
+    Some((l,r)) => println!("{} {}", l, r),
+    None => println!("-1"),
+  }
+}
