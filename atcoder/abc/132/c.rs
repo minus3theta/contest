23d223aa587c92ad069efd3d02a15e274fc731e5
minus3theta <yone_j_synthesis@yahoo.co.jp>
2019-06-30 23:06:53 +0900

    Add ABC 132 A, B, C, D, E

---
+++ atcoder/abc/132/c.rs
@@ -0,0 +1,74 @@
+#[allow(unused_imports)]
+use std::cmp;
+
+// Input macro from https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+  (source = $s:expr, $($r:tt)*) => {
+    let mut iter = $s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+  ($($r:tt)*) => {
+    let s = {
+      use std::io::Read;
+      let mut s = String::new();
+      std::io::stdin().read_to_string(&mut s).unwrap();
+      s
+    };
+    let mut iter = s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+}
+
+macro_rules! input_inner {
+  ($iter:expr) => {};
+  ($iter:expr, ) => {};
+
+  ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+    let $var = read_value!($iter, $t);
+    input_inner!{$iter $($r)*}
+  };
+}
+
+macro_rules! read_value {
+  ($iter:expr, ( $($t:tt),* )) => {
+    ( $(read_value!($iter, $t)),* )
+  };
+
+  ($iter:expr, [ $t:tt ; $len:expr ]) => {
+    (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+  };
+
+  ($iter:expr, chars) => {
+    read_value!($iter, String).chars().collect::<Vec<char>>()
+  };
+
+  ($iter:expr, usize1) => {
+    read_value!($iter, usize) - 1
+  };
+
+  ($iter:expr, $t:ty) => {
+    $iter.next().unwrap().parse::<$t>().expect("Parse error")
+  };
+}
+
+fn main() {
+  input! {
+    n: usize,
+    ds: [usize; n],
+  }
+  let m = n / 2;
+  let mut pop = vec![0; 100_001];
+  for &d in &ds {
+    pop[d] += 1;
+  }
+  let mut ans = 0;
+  let mut sum = 0;
+  for k in 0 .. 100_001 {
+    if sum == m {
+      ans += 1;
+    }
+    sum += pop[k];
+  }
+
+  println!("{}", ans);
+}
---
+++ atcoder/abc/132/e.rs
@@ -0,0 +1,152 @@
+#[allow(unused_imports)]
+use std::cmp;
+
+// Input macro from https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+  (source = $s:expr, $($r:tt)*) => {
+    let mut iter = $s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+  ($($r:tt)*) => {
+    let s = {
+      use std::io::Read;
+      let mut s = String::new();
+      std::io::stdin().read_to_string(&mut s).unwrap();
+      s
+    };
+    let mut iter = s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+}
+
+macro_rules! input_inner {
+  ($iter:expr) => {};
+  ($iter:expr, ) => {};
+
+  ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+    let $var = read_value!($iter, $t);
+    input_inner!{$iter $($r)*}
+  };
+}
+
+macro_rules! read_value {
+  ($iter:expr, ( $($t:tt),* )) => {
+    ( $(read_value!($iter, $t)),* )
+  };
+
+  ($iter:expr, [ $t:tt ; $len:expr ]) => {
+    (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+  };
+
+  ($iter:expr, chars) => {
+    read_value!($iter, String).chars().collect::<Vec<char>>()
+  };
+
+  ($iter:expr, usize1) => {
+    read_value!($iter, usize) - 1
+  };
+
+  ($iter:expr, $t:ty) => {
+    $iter.next().unwrap().parse::<$t>().expect("Parse error")
+  };
+}
+
+use std::collections::BTreeSet;
+
+fn main() {
+  input! {
+    n: usize,
+    m: usize,
+    es: [(usize1, usize1); m],
+    s: usize1,
+    t: usize1,
+  }
+  let mut vs = vec![vec![]; n * 3];
+  for (u, v) in es.into_iter() {
+    vs[u * 3].push(Edge{node: v * 3 + 1, cost: 1});
+    vs[u * 3 + 1].push(Edge{node: v * 3 + 2, cost: 1});
+    vs[u * 3 + 2].push(Edge{node: v * 3, cost: 1});
+  }
+  let sp = shortest_path(&vs, s * 3, t * 3);
+  match sp {
+    None => println!("-1"),
+    Some(d) => println!("{}", d / 3),
+  }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct State {
+    cost: usize,
+    position: usize,
+}
+
+// The priority queue depends on `Ord`.
+// Explicitly implement the trait so the queue becomes a min-heap
+// instead of a max-heap.
+impl Ord for State {
+    fn cmp(&self, other: &State) -> Ordering {
+        // Notice that the we flip the ordering on costs.
+        // In case of a tie we compare positions - this step is necessary
+        // to make implementations of `PartialEq` and `Ord` consistent.
+        other.cost.cmp(&self.cost)
+    }
+}
+
+// `PartialOrd` needs to be implemented as well.
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &State) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+// Each node is represented as an `usize`, for a shorter implementation.
+#[derive(Clone)]
+struct Edge {
+    node: usize,
+    cost: usize,
+}
+
+use std::collections::BinaryHeap;
+use cmp::Ordering;
+use std::usize;
+// Dijkstra's shortest path algorithm.
+
+// Start at `start` and use `dist` to track the current shortest distance
+// to each node. This implementation isn't memory-efficient as it may leave duplicate
+// nodes in the queue. It also uses `usize::MAX` as a sentinel value,
+// for a simpler implementation.
+fn shortest_path(adj_list: &Vec<Vec<Edge>>, start: usize, goal: usize) -> Option<usize> {
+    // dist[node] = current shortest distance from `start` to `node`
+    let mut dist: Vec<_> = (0..adj_list.len()).map(|_| usize::MAX).collect();
+
+    let mut heap = BinaryHeap::new();
+
+    // We're at `start`, with a zero cost
+    dist[start] = 0;
+    heap.push(State { cost: 0, position: start });
+
+    // Examine the frontier with lower cost nodes first (min-heap)
+    while let Some(State { cost, position }) = heap.pop() {
+        // Alternatively we could have continued to find all shortest paths
+        if position == goal { return Some(cost); }
+
+        // Important as we may have already found a better way
+        if cost > dist[position] { continue; }
+
+        // For each node we can reach, see if we can find a way with
+        // a lower cost going through this node
+        for edge in &adj_list[position] {
+            let next = State { cost: cost + edge.cost, position: edge.node };
+
+            // If so, add it to the frontier and continue
+            if next.cost < dist[next.position] {
+                heap.push(next);
+                // Relaxation, we have now found a better way
+                dist[next.position] = next.cost;
+            }
+        }
+    }
+
+    // Goal not reachable
+    None
+}
