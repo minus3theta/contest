39e33f8a0f1dde92a396366af4e781934698b7ef
minus3theta <yone_j_synthesis@yahoo.co.jp>
2019-07-08 03:32:50 +0900

    Add ABC 133 A, B, C, D, E

---
+++ atcoder/abc/133/c.rs
@@ -0,0 +1,79 @@
+#[allow(unused_imports)]
+use std::cmp;
+use std::io::Write;
+
+// Input macro from https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+  (source = $s:expr, $($r:tt)*) => {
+    let mut iter = $s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+  ($($r:tt)*) => {
+    let s = {
+      use std::io::Read;
+      let mut s = String::new();
+      std::io::stdin().read_to_string(&mut s).unwrap();
+      s
+    };
+    let mut iter = s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+}
+
+macro_rules! input_inner {
+  ($iter:expr) => {};
+  ($iter:expr, ) => {};
+
+  ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+    let $var = read_value!($iter, $t);
+    input_inner!{$iter $($r)*}
+  };
+}
+
+macro_rules! read_value {
+  ($iter:expr, ( $($t:tt),* )) => {
+    ( $(read_value!($iter, $t)),* )
+  };
+
+  ($iter:expr, [ $t:tt ; $len:expr ]) => {
+    (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+  };
+
+  ($iter:expr, chars) => {
+    read_value!($iter, String).chars().collect::<Vec<char>>()
+  };
+
+  ($iter:expr, usize1) => {
+    read_value!($iter, usize) - 1
+  };
+
+  ($iter:expr, $t:ty) => {
+    $iter.next().unwrap().parse::<$t>().expect("Parse error")
+  };
+}
+
+fn main() {
+  let out = std::io::stdout();
+  let mut out = std::io::BufWriter::new(out.lock());
+  macro_rules! puts {
+    ($($format:tt)*) => (write!(out,$($format)*).unwrap());
+  }
+  input! {
+    l: i64,
+    r: i64,
+  }
+  let m = 2019;
+  let a = r / m * m;
+  if l <= a {
+    puts!("0\n");
+    return;
+  }
+  let mut ans = m;
+  for i in l .. r + 1 {
+    for j in i + 1 .. r + 1 {
+      ans = cmp::min(ans, i * j % m);
+    }
+  }
+
+  puts!("{}\n", ans);
+}
