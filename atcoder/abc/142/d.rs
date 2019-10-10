b7da7c9945d030a164b45fe16c9795fa200d0eb8
minus3theta <yone_j_synthesis@yahoo.co.jp>
2019-10-10 20:19:19 +0900

    Add ABC 142 D, E

---
+++ atcoder/abc/142/d.rs
@@ -0,0 +1,90 @@
+#[allow(unused_imports)]
+use std::cmp;
+use std::io::Write;
+
+// Input macro from https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+  (source = $s:expr, $($r:tt)*) => {
+    let mut iter = $s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+  ($($r:tt)*) => {
+    let s = {
+      use std::io::Read;
+      let mut s = String::new();
+      std::io::stdin().read_to_string(&mut s).unwrap();
+      s
+    };
+    let mut iter = s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+}
+
+macro_rules! input_inner {
+  ($iter:expr) => {};
+  ($iter:expr, ) => {};
+
+  ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+    let $var = read_value!($iter, $t);
+    input_inner!{$iter $($r)*}
+  };
+}
+
+macro_rules! read_value {
+  ($iter:expr, ( $($t:tt),* )) => {
+    ( $(read_value!($iter, $t)),* )
+  };
+
+  ($iter:expr, [ $t:tt ; $len:expr ]) => {
+    (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+  };
+
+  ($iter:expr, chars) => {
+    read_value!($iter, String).chars().collect::<Vec<char>>()
+  };
+
+  ($iter:expr, usize1) => {
+    read_value!($iter, usize) - 1
+  };
+
+  ($iter:expr, $t:ty) => {
+    $iter.next().unwrap().parse::<$t>().expect("Parse error")
+  };
+}
+
+fn gcd(x: i64, y: i64) -> i64 {
+  if y == 0 {
+    x
+  } else {
+    gcd(y, x % y)
+  }
+}
+
+fn main() {
+  let out = std::io::stdout();
+  let mut out = std::io::BufWriter::new(out.lock());
+  macro_rules! puts {
+    ($($format:tt)*) => (write!(out,$($format)*).unwrap());
+  }
+  input! {
+    a: i64,
+    b: i64,
+  }
+  let mut g = gcd(a, b);
+  let mut ans = 1;
+  let mut div = 2;
+  while div * div <= g {
+    if g % div == 0 {
+      ans += 1;
+      while g % div == 0 {
+        g /= div;
+      }
+    }
+    div += 1;
+  }
+  if g != 1 {
+    ans += 1;
+  }
+
+  puts!("{}\n", ans);
+}
---
+++ atcoder/abc/142/e.rs
@@ -0,0 +1,88 @@
+#[allow(unused_imports)]
+use std::cmp;
+use std::io::Write;
+
+// Input macro from https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+  (source = $s:expr, $($r:tt)*) => {
+    let mut iter = $s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+  ($($r:tt)*) => {
+    let s = {
+      use std::io::Read;
+      let mut s = String::new();
+      std::io::stdin().read_to_string(&mut s).unwrap();
+      s
+    };
+    let mut iter = s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+}
+
+macro_rules! input_inner {
+  ($iter:expr) => {};
+  ($iter:expr, ) => {};
+
+  ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+    let $var = read_value!($iter, $t);
+    input_inner!{$iter $($r)*}
+  };
+}
+
+macro_rules! read_value {
+  ($iter:expr, ( $($t:tt),* )) => {
+    ( $(read_value!($iter, $t)),* )
+  };
+
+  ($iter:expr, [ $t:tt ; $len:expr ]) => {
+    (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+  };
+
+  ($iter:expr, chars) => {
+    read_value!($iter, String).chars().collect::<Vec<char>>()
+  };
+
+  ($iter:expr, usize1) => {
+    read_value!($iter, usize) - 1
+  };
+
+  ($iter:expr, [ $t:tt ]) => {{
+    let len = read_value!($iter, usize);
+    (0..len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+  }};
+
+  ($iter:expr, $t:ty) => {
+    $iter.next().unwrap().parse::<$t>().expect("Parse error")
+  };
+}
+
+const INF: i64 = 1 << 60;
+
+fn main() {
+  let out = std::io::stdout();
+  let mut out = std::io::BufWriter::new(out.lock());
+  macro_rules! puts {
+    ($($format:tt)*) => (write!(out,$($format)*).unwrap());
+  }
+  input! {
+    n: usize,
+    m: usize,
+    key: [(i64, [usize1]); m],
+  }
+  let key: Vec<_> = key.into_iter().map(|(a, c)| (a, c.into_iter().map(|x| 1 << x).sum::<usize>())).collect();
+  let mut cost = vec![INF; 1 << n];
+  cost[0] = 0;
+  for i in 0 .. 1 << n {
+    for &(a, c) in &key {
+      cost[i | c] = cmp::min(cost[i | c], cost[i] + a);
+    }
+  }
+  let ans = if cost[(1 << n) - 1] == INF {
+    -1
+  } else {
+    cost[(1 << n) - 1]
+  };
+
+  puts!("{}\n", ans);
+}
