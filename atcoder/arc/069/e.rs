1f1d30c96c5c8959a6d942901f5f9c97fd129d02
minus3theta <yone_j_synthesis@yahoo.co.jp>
2019-10-30 12:23:53 +0900

    Add ARC 069 E

---
+++ atcoder/arc/069/e.rs
@@ -0,0 +1,93 @@
+#[allow(unused_imports)]
+use std::cmp;
+use std::io::Write;
+
+// Input macro from https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+  (source = $s:expr, $($r:tt)*) => {
+    let mut iter = $s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+  ($($r:tt)*) => {
+    let s = {
+      use std::io::Read;
+      let mut s = String::new();
+      std::io::stdin().read_to_string(&mut s).unwrap();
+      s
+    };
+    let mut iter = s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+}
+
+macro_rules! input_inner {
+  ($iter:expr) => {};
+  ($iter:expr, ) => {};
+
+  ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+    let $var = read_value!($iter, $t);
+    input_inner!{$iter $($r)*}
+  };
+}
+
+macro_rules! read_value {
+  ($iter:expr, ( $($t:tt),* )) => {
+    ( $(read_value!($iter, $t)),* )
+  };
+
+  ($iter:expr, [ $t:tt ; $len:expr ]) => {
+    (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+  };
+
+  ($iter:expr, chars) => {
+    read_value!($iter, String).chars().collect::<Vec<char>>()
+  };
+
+  ($iter:expr, usize1) => {
+    read_value!($iter, usize) - 1
+  };
+
+  ($iter:expr, [ $t:tt ]) => {{
+    let len = read_value!($iter, usize);
+    (0..len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+  }};
+
+  ($iter:expr, $t:ty) => {
+    $iter.next().unwrap().parse::<$t>().expect("Parse error")
+  };
+}
+
+use std::collections::BTreeMap;
+
+fn main() {
+  let out = std::io::stdout();
+  let mut out = std::io::BufWriter::new(out.lock());
+  macro_rules! puts {
+    ($($format:tt)*) => (write!(out,$($format)*).unwrap());
+  }
+  input! {
+    n: usize,
+    a: [i64; n],
+  }
+  let mut map = BTreeMap::new();
+  let mut a: Vec<_> = a.into_iter().enumerate().map(|(i, x)| (x, i)).collect();
+  a.sort();
+  let mut mini = n;
+  for &(x, i) in a.iter().rev() {
+    mini = cmp::min(mini, i);
+    let e = map.entry(x).or_insert((i, 0));
+    e.0 = cmp::min(e.0, mini);
+    e.1 += 1;
+  }
+  let mut ans = vec![0; n];
+  let mut prev = 0;
+  let mut count = n;
+  for (&size, &(front, c)) in &map {
+    ans[front] += count as i64 * (size - prev);
+    prev = size;
+    count -= c;
+  }
+  for &x in &ans {
+    puts!("{}\n", x);
+  }
+}
