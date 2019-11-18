a6cb9fbbbc9828876d3f81a366d783c143f391dd
minus3theta <yone_j_synthesis@yahoo.co.jp>
2019-11-18 18:27:56 +0900

    Add ABC 145 D, E, F

---
+++ atcoder/abc/145/d.rs
@@ -0,0 +1,178 @@
+#[allow(unused_imports)]
+use std::cmp;
+use std::io::Write;
+
+// Input macro from https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+  (source = $s:expr, $($r:tt)*) => {
+    let mut iter = $s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+  ($($r:tt)*) => {
+    let s = {
+      use std::io::Read;
+      let mut s = String::new();
+      std::io::stdin().read_to_string(&mut s).unwrap();
+      s
+    };
+    let mut iter = s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+}
+
+macro_rules! input_inner {
+  ($iter:expr) => {};
+  ($iter:expr, ) => {};
+
+  ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+    let $var = read_value!($iter, $t);
+    input_inner!{$iter $($r)*}
+  };
+}
+
+macro_rules! read_value {
+  ($iter:expr, ( $($t:tt),* )) => {
+    ( $(read_value!($iter, $t)),* )
+  };
+
+  ($iter:expr, [ $t:tt ; $len:expr ]) => {
+    (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+  };
+
+  ($iter:expr, chars) => {
+    read_value!($iter, String).chars().collect::<Vec<char>>()
+  };
+
+  ($iter:expr, usize1) => {
+    read_value!($iter, usize) - 1
+  };
+
+  ($iter:expr, [ $t:tt ]) => {{
+    let len = read_value!($iter, usize);
+    (0..len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+  }};
+
+  ($iter:expr, $t:ty) => {
+    $iter.next().unwrap().parse::<$t>().expect("Parse error")
+  };
+}
+
+const MOD: i64 = 1_000_000_007;
+
+#[derive(Copy, Clone, Hash, Debug)]
+struct ModInt {
+  x: i64
+}
+impl ModInt {
+  fn new(x: i64) -> Self {
+    ModInt {
+      x: x % MOD
+    }
+  }
+  fn pow(self, e: i64) -> Self {
+    if e == 0 {
+      Self::new(1)
+    } else if e % 2 == 1 {
+      self.pow(e - 1) * self
+    } else {
+      let y = self.pow(e / 2);
+      y * y
+    }
+  }
+  #[allow(dead_code)]
+  fn inv(self) -> Self {
+    self.pow(MOD - 2)
+  }
+}
+impl<T: Into<ModInt>> std::ops::Add<T> for ModInt {
+  type Output = Self;
+  fn add(self, other: T) -> Self {
+    let other = other.into();
+    let sum = self.x + other.x;
+    ModInt {
+      x: if sum >= MOD { sum - MOD } else { sum }
+    }
+  }
+}
+impl<T: Into<ModInt>> std::ops::Sub<T> for ModInt {
+  type Output = Self;
+  fn sub(self, other: T) -> Self {
+    let other = other.into();
+    let sum = self.x - other.x;
+    ModInt {
+      x: if sum < 0 { sum + MOD } else { sum }
+    }
+  }
+}
+impl<T: Into<ModInt>> std::ops::Mul<T> for ModInt {
+  type Output = Self;
+  fn mul(self, other: T) -> Self {
+    let other = other.into();
+    ModInt {
+      x: self.x * other.x % MOD
+    }
+  }
+}
+impl<T: Into<ModInt>> std::ops::Div<T> for ModInt {
+  type Output = Self;
+  fn div(self, other: T) -> Self {
+    let other = other.into();
+    self * other.inv()
+  }
+}
+impl From<i64> for ModInt {
+  fn from(x: i64) -> Self {
+    ModInt::new(x)
+  }
+}
+impl std::fmt::Display for ModInt {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    self.x.fmt(f)
+  }
+}
+impl<T: Into<ModInt>> std::ops::AddAssign<T> for ModInt {
+  fn add_assign(&mut self, other: T) {
+    *self = *self + other;
+  }
+}
+impl<T: Into<ModInt>> std::ops::SubAssign<T> for ModInt {
+  fn sub_assign(&mut self, other: T) {
+    *self = *self - other;
+  }
+}
+impl<T: Into<ModInt>> std::ops::MulAssign<T> for ModInt {
+  fn mul_assign(&mut self, other: T) {
+    *self = *self * other;
+  }
+}
+impl<T: Into<ModInt>> std::ops::DivAssign<T> for ModInt {
+  fn div_assign(&mut self, other: T) {
+    *self = *self / other;
+  }
+}
+
+fn main() {
+  let out = std::io::stdout();
+  let mut out = std::io::BufWriter::new(out.lock());
+  macro_rules! puts {
+    ($($format:tt)*) => (write!(out,$($format)*).unwrap());
+  }
+  input! {
+    x: i32,
+    y: i32,
+  }
+  let p = 2 * y - x;
+  let q = 2 * x - y;
+  if p % 3 != 0 || p < 0 || q % 3 != 0 || q < 0 {
+    puts!("0\n");
+    return;
+  }
+  let p = (p / 3) as usize;
+  let q = (q / 3) as usize;
+  let mut fact = vec![ModInt::new(0); p + q + 1];
+  fact[0] = 1.into();
+  for i in 1 .. p + q + 1 {
+    fact[i] = fact[i-1] * ModInt::new(i as i64);
+  }
+  puts!("{}\n", fact[p + q] * fact[p].inv() * fact[q].inv());
+}
---
+++ atcoder/abc/145/e.rs
@@ -0,0 +1,88 @@
+#[allow(unused_imports)]
+use std::cmp;
+use std::io::Write;
+
+// Input macro from https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+  (source = $s:expr, $($r:tt)*) => {
+    let mut iter = $s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+  ($($r:tt)*) => {
+    let s = {
+      use std::io::Read;
+      let mut s = String::new();
+      std::io::stdin().read_to_string(&mut s).unwrap();
+      s
+    };
+    let mut iter = s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+}
+
+macro_rules! input_inner {
+  ($iter:expr) => {};
+  ($iter:expr, ) => {};
+
+  ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+    let $var = read_value!($iter, $t);
+    input_inner!{$iter $($r)*}
+  };
+}
+
+macro_rules! read_value {
+  ($iter:expr, ( $($t:tt),* )) => {
+    ( $(read_value!($iter, $t)),* )
+  };
+
+  ($iter:expr, [ $t:tt ; $len:expr ]) => {
+    (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+  };
+
+  ($iter:expr, chars) => {
+    read_value!($iter, String).chars().collect::<Vec<char>>()
+  };
+
+  ($iter:expr, usize1) => {
+    read_value!($iter, usize) - 1
+  };
+
+  ($iter:expr, [ $t:tt ]) => {{
+    let len = read_value!($iter, usize);
+    (0..len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+  }};
+
+  ($iter:expr, $t:ty) => {
+    $iter.next().unwrap().parse::<$t>().expect("Parse error")
+  };
+}
+
+fn main() {
+  let out = std::io::stdout();
+  let mut out = std::io::BufWriter::new(out.lock());
+  macro_rules! puts {
+    ($($format:tt)*) => (write!(out,$($format)*).unwrap());
+  }
+  input! {
+    n: usize,
+    t: usize,
+    ab: [(usize, i32); n],
+  }
+  let mut ab = ab;
+  ab.sort();
+  let mut dp = vec![vec![0; t]; n];
+  let mut ans = 0;
+  for (i, &(a, b)) in ab.iter().take(n-1).enumerate() {
+    let mut current = 0;
+    for j in 0 .. t {
+      dp[i+1][j] = match j.checked_sub(a) {
+        None => dp[i][j],
+        Some(v) => cmp::max(dp[i][j], dp[i][v] + b),
+      };
+      current = cmp::max(current, dp[i+1][j]);
+    }
+    ans = cmp::max(ans, current + ab[i+1].1);
+  }
+
+  puts!("{}\n", ans);
+}
