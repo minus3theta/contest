50336223be10d2761129d08738ef9caaa4b6d6d2
minus3theta <yone_j_synthesis@yahoo.co.jp>
2019-12-02 03:30:58 +0900

    Add Sumitrust 2019 A, B, C, D, E, F

---
+++ atcoder/others/sumitrust2019/d.rs
@@ -0,0 +1,89 @@
+#[allow(unused_imports)]
+use std::cmp;
+use std::io::Write;
+
+// Input macro from https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+  (source = $s:expr, $($r:tt)*) => {
+    let mut iter = $s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+  ($($r:tt)*) => {
+    let s = {
+      use std::io::Read;
+      let mut s = String::new();
+      std::io::stdin().read_to_string(&mut s).unwrap();
+      s
+    };
+    let mut iter = s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+}
+
+macro_rules! input_inner {
+  ($iter:expr) => {};
+  ($iter:expr, ) => {};
+
+  ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+    let $var = read_value!($iter, $t);
+    input_inner!{$iter $($r)*}
+  };
+}
+
+macro_rules! read_value {
+  ($iter:expr, ( $($t:tt),* )) => {
+    ( $(read_value!($iter, $t)),* )
+  };
+
+  ($iter:expr, [ $t:tt ; $len:expr ]) => {
+    (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+  };
+
+  ($iter:expr, chars) => {
+    read_value!($iter, String).chars().collect::<Vec<char>>()
+  };
+
+  ($iter:expr, usize1) => {
+    read_value!($iter, usize) - 1
+  };
+
+  ($iter:expr, [ $t:tt ]) => {{
+    let len = read_value!($iter, usize);
+    (0..len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+  }};
+
+  ($iter:expr, $t:ty) => {
+    $iter.next().unwrap().parse::<$t>().expect("Parse error")
+  };
+}
+
+fn main() {
+  let out = std::io::stdout();
+  let mut out = std::io::BufWriter::new(out.lock());
+  macro_rules! puts {
+    ($($format:tt)*) => (write!(out,$($format)*).unwrap());
+  }
+  input! {
+    _n: usize,
+    s: chars,
+  }
+  let mut ans = 0;
+  for p in 0 .. 1000 {
+    let d = vec![p / 100, p / 10 % 10, p % 10];
+    let mut i = 0;
+    for &c in &s {
+      let c = c as i32 - '0' as i32;
+      if c == d[i] {
+        i += 1;
+      }
+      if i == 3 {
+        break;
+      }
+    }
+    if i == 3 {
+      ans += 1;
+    }
+  }
+
+  puts!("{}\n", ans);
+}
