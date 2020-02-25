94fdff0b24763c611fe790f3943ad9868848212a
minus3theta <yone_j_synthesis@yahoo.co.jp>
2020-02-25 20:09:17 +0900

    Add ABC 155 E

---
+++ cargo/abc155/src/bin/e.rs
@@ -0,0 +1,89 @@
+#[allow(unused_imports)]
+use std::cmp;
+use std::io::Write;
+
+// Input macro from https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+  (source = $s:expr, $($r:tt)*) => {
+    let mut iter = $s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+  ($($r:tt)*) => {
+    let s = {
+      use std::io::Read;
+      let mut s = String::new();
+      std::io::stdin().read_to_string(&mut s).unwrap();
+      s
+    };
+    let mut iter = s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+}
+
+macro_rules! input_inner {
+  ($iter:expr) => {};
+  ($iter:expr, ) => {};
+
+  ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+    let $var = read_value!($iter, $t);
+    input_inner!{$iter $($r)*}
+  };
+}
+
+macro_rules! read_value {
+  ($iter:expr, ( $($t:tt),* )) => {
+    ( $(read_value!($iter, $t)),* )
+  };
+
+  ($iter:expr, [ $t:tt ; $len:expr ]) => {
+    (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+  };
+
+  ($iter:expr, chars) => {
+    read_value!($iter, String).chars().collect::<Vec<char>>()
+  };
+
+  ($iter:expr, usize1) => {
+    read_value!($iter, usize) - 1
+  };
+
+  ($iter:expr, [ $t:tt ]) => {{
+    let len = read_value!($iter, usize);
+    (0..len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+  }};
+
+  ($iter:expr, $t:ty) => {
+    $iter.next().unwrap().parse::<$t>().expect("Parse error")
+  };
+}
+
+fn main() {
+  let out = std::io::stdout();
+  let mut out = std::io::BufWriter::new(out.lock());
+  macro_rules! puts {
+    ($($format:tt)*) => (write!(out,$($format)*).unwrap());
+  }
+  input! {
+    n: chars,
+  }
+  let n: Vec<_> = n.into_iter().map(|d| d as i32 - '0' as i32).collect();
+  let l = n.len();
+  let mut same = vec![0; l+1];
+  let mut more = vec![0; l+1];
+  same[0] = 0;
+  more[0] = 1;
+  for i in 1 .. l + 1 {
+    same[i] = cmp::min(
+      same[i-1] + n[i-1],
+      more[i-1] + 10 - n[i-1]
+    );
+    more[i] = cmp::min(
+      same[i-1] + n[i-1] + 1,
+      more[i-1] + 9 - n[i-1]
+    );
+  }
+  // dbg!(&same);
+  // dbg!(&more);
+
+  puts!("{}\n", same[l]);
+}
