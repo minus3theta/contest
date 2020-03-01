d4e2440804044ac3eaa44cb40fe2b215aeed8f79
minus3theta <yone_j_synthesis@yahoo.co.jp>
2020-03-02 03:07:05 +0900

    Add Circle to geometry.rs

--- lib/geometry.rs
+++ lib/geometry.rs
@@ -15,6 +15,7 @@ impl Point {
     }
   }
   #[allow(dead_code)]
+  // divide a segment into; ratio : 1 - ratio
   fn division(self, other: Self, ratio: f64) -> Self {
     self + (other - self) * ratio
   }
@@ -24,12 +25,7 @@ impl Point {
   }
   #[allow(dead_code)]
   fn line_with_normal(self, vec: Vect) -> Line {
-    let vec = vec.normalized();
-    Line {
-      a: vec.x,
-      b: vec.y,
-      c: -self.x * vec.x - self.y * vec.y,
-    }
+    Line::new(vec.x, vec.y, -self.x * vec.x - self.y * vec.y)
   }
   #[allow(dead_code)]
   fn line_toward(self, vec: Vect) -> Line {
@@ -43,6 +39,27 @@ impl Point {
   fn bisector(self, other: Point) -> Line {
     let midpoint = self.division(other, 0.5);
     midpoint.line_with_normal(other - self)
+  }
+  #[allow(dead_code)]
+  fn dist_to_line(self, line: Line) -> f64 {
+    (line.a * self.x + line.b * self.y + line.c).abs()
+  }
+  #[allow(dead_code)]
+  fn apollonius(self, other: Point, r1: f64, r2: f64) -> Option<Curve> {
+    use Curve::*;
+    if self.dist(other) < EPS || r1 < EPS || r2 < EPS {
+      return None;
+    }
+    if (r1 - r2).abs() < EPS {
+      return Some(CLine(self.bisector(other)));
+    }
+    let p1 = self.division(other, r1 / (r1 + r2));
+    let p2 = self.division(other, r1 / (r1 - r2));
+    Some(CCircle(Circle::new(p1.division(p2, 0.5), p1.dist(p2) / 2.0)))
+  }
+  #[allow(dead_code)]
+  fn perpendicular(self, line: Line) -> Line {
+    self.line_toward(line.normal_vect())
   }
 }
 impl<T: Into<Vect>> std::ops::Add<T> for Point {
@@ -189,10 +206,11 @@ struct Line {
 impl Line {
   #[allow(dead_code)]
   fn new(a: f64, b: f64, c: f64) -> Self {
+    let scale = (a.powi(2) + b.powi(2)).sqrt();
     Line {
-      a: a,
-      b: b,
-      c: c,
+      a: a / scale,
+      b: b / scale,
+      c: c / scale,
     }
   }
   #[allow(dead_code)]
@@ -205,5 +223,76 @@ impl Line {
       x: (-other.b * self.c + self.b * other.c) / det,
       y: (other.a * self.c - self.a * other.c) / det,
     })
+  }
+  #[allow(dead_code)]
+  fn normal_vect(self) -> Vect {
+    Vect::new(self.a, self.b)
+  }
+}
+
+#[derive(Copy, Clone, Debug)]
+struct Circle {
+  center: Point,
+  radius: f64,
+}
+impl Circle {
+  #[allow(dead_code)]
+  fn new(center: Point, radius: f64) -> Self {
+    Circle {
+      center: center,
+      radius: radius
+    }
+  }
+  #[allow(dead_code)]
+  fn intersection_with_line(self, line: Line) -> Vec<Point> {
+    let perpendicular = self.center.perpendicular(line);
+    let foot = line.intersection(perpendicular).unwrap();
+    let dist = self.center.dist_to_line(line);
+    if (dist - self.radius).abs() < EPS {
+      return vec![foot];
+    }
+    if dist > self.radius {
+      return vec![];
+    }
+    let base = (self.radius.powi(2) - dist.powi(2)).sqrt();
+    let v = perpendicular.normal_vect() * base;
+    vec![foot + v, foot - v]
+  }
+  #[allow(dead_code)]
+  fn intersection_with_circle(self, other: Circle) -> Vec<Point> {
+    let dist = self.center.dist(other.center);
+    if dist < EPS {
+      return vec![];
+    }
+    fn diff(c: Circle) -> f64 {
+      c.radius.powi(2) - c.center.x.powi(2) - c.center.y.powi(2)
+    }
+    let c = (diff(self) - diff(other)) / 2.0;
+    let v = self.center - other.center;
+    let line = Line::new(v.x, v.y, c);
+    self.intersection_with_line(line)
+  }
+}
+
+#[derive(Copy, Clone, Debug)]
+enum Curve {
+  CLine(Line),
+  CCircle(Circle),
+}
+impl Curve {
+  #[allow(dead_code)]
+  fn intersection(self, other: Curve) -> Vec<Point> {
+    use Curve::*;
+    match (self, other) {
+      (CLine(l1), CLine(l2)) => {
+        l1.intersection(l2).into_iter().collect()
+      },
+      (CLine(l), CCircle(c)) | (CCircle(c), CLine(l)) => {
+        c.intersection_with_line(l)
+      },
+      (CCircle(c1), CCircle(c2)) => {
+        c1.intersection_with_circle(c2)
+      }
+    }
   }
 }
