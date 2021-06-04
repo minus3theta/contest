38955d3eabdf4f8347d4e9a7c28f8a5f1a0380a3
minus3theta <yone_j_synthesis@yahoo.co.jp>
2021-06-05 02:20:12 +0900

    Add typical90 020

---
+++ cargo/typical90/src/bin/020.rs
@@ -0,0 +1,25 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use num::Integer;
+#[allow(unused_imports)]
+use proconio::derive_readable;
+use proconio::fastout;
+use proconio::input;
+#[allow(unused_imports)]
+use proconio::marker::*;
+#[allow(unused_imports)]
+use std::cmp;
+#[allow(unused_imports)]
+use std::collections::*;
+
+#[fastout]
+fn main() {
+    input! {
+        a: i64,
+        b: u32,
+        c: i64,
+    }
+
+    println!("{}", if a < c.pow(b) { "Yes" } else { "No" });
+}
