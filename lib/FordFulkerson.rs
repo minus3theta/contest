f53feeaf37d8a57699986ec0dbceaf4c12712bee
minus3theta <yone_j_synthesis@yahoo.co.jp>
2021-07-03 15:07:29 +0900

    Refactor Ford-Fulkerson

--- lib/FordFulkerson.rs
+++ lib/FordFulkerson.rs
@@ -1,55 +1,131 @@
 use std::cell::Cell;
 
 #[derive(Debug, Clone)]
-struct Edge {
+pub struct Edge<E> {
     pub to: usize,
     pub cap: Cell<i64>,
+    pub weight: E,
     pub rev: usize,
 }
 
-fn add_edge(from: usize, to: usize, cap: i64, es: &mut Vec<Vec<Edge>>) {
-    let from_len = es[from].len();
-    let to_len = es[to].len();
-    es[from].push(Edge {
-        to,
-        cap: Cell::new(cap),
-        rev: to_len,
-    });
-    es[to].push(Edge {
-        to: from,
-        cap: Cell::new(0),
-        rev: from_len,
-    });
+#[derive(Debug, Clone)]
+pub struct FlowGraph<E> {
+    pub edges: Vec<Vec<Edge<E>>>,
 }
 
-fn dfs(v: usize, t: usize, f: i64, es: &Vec<Vec<Edge>>, used: &mut Vec<bool>) -> i64 {
-    if v == t {
-        return f;
+impl<E> FlowGraph<E> {
+    pub fn new(n: usize) -> Self {
+        Self {
+            edges: (0..n).map(|_| Vec::new()).collect(),
+        }
     }
-    used[v] = true;
-    for e in &es[v] {
-        let cap = e.cap.get();
-        if !used[e.to] && cap > 0 {
-            let d = dfs(e.to, t, f.min(cap), es, used);
-            if d > 0 {
-                e.cap.set(cap - d);
-                let r_cap = es[e.to][e.rev].cap.get();
-                es[e.to][e.rev].cap.set(r_cap + d);
-                return d;
+    pub fn len(&self) -> usize {
+        self.edges.len()
+    }
+}
+
+impl<E: Clone> FlowGraph<E> {
+    pub fn add_edge(&mut self, from: usize, to: usize, cap: i64, weight: E) {
+        let from_len = self.edges[from].len();
+        let to_len = self.edges[to].len();
+        self.edges[from].push(Edge {
+            to,
+            cap: Cell::new(cap),
+            weight: weight.clone(),
+            rev: to_len,
+        });
+        self.edges[to].push(Edge {
+            to: from,
+            cap: Cell::new(0),
+            weight,
+            rev: from_len,
+        });
+    }
+}
+
+pub trait MaxFlow<E> {
+    fn max_flow(graph: &FlowGraph<E>, s: usize, t: usize) -> i64;
+}
+
+pub enum FordFulkerson {}
+
+impl<E> MaxFlow<E> for FordFulkerson {
+    fn max_flow(graph: &FlowGraph<E>, s: usize, t: usize) -> i64 {
+        let mut flow = 0;
+        loop {
+            let mut used = vec![false; graph.edges.len()];
+            let f = Self::dfs(graph, s, t, std::i64::MAX, &mut used);
+            if f == 0 {
+                break flow;
+            }
+            flow += f;
+        }
+    }
+}
+
+impl FordFulkerson {
+    fn dfs<E>(graph: &FlowGraph<E>, v: usize, t: usize, f: i64, used: &mut Vec<bool>) -> i64 {
+        if v == t {
+            return f;
+        }
+        used[v] = true;
+        for e in &graph.edges[v] {
+            let cap = e.cap.get();
+            if !used[e.to] && cap > 0 {
+                let d = Self::dfs(graph, e.to, t, f.min(cap), used);
+                if d > 0 {
+                    e.cap.set(cap - d);
+                    let r_cap = graph.edges[e.to][e.rev].cap.get();
+                    graph.edges[e.to][e.rev].cap.set(r_cap + d);
+                    return d;
+                }
             }
         }
+        0
     }
-    0
 }
 
-fn max_flow(s: usize, t: usize, es: &Vec<Vec<Edge>>) -> i64 {
-    let mut flow = 0;
-    loop {
-        let mut used = vec![false; es.len()];
-        let f = dfs(s, t, 1 << 60, es, &mut used);
-        if f == 0 {
-            break flow;
+#[derive(Debug, Clone)]
+pub struct BipartiteMatching<E> {
+    pub graph: FlowGraph<E>,
+    pub a: usize,
+    pub b: usize,
+    pub source: usize,
+    pub sink: usize,
+}
+
+impl<E: Clone + Default> BipartiteMatching<E> {
+    pub fn new(a: usize, b: usize) -> Self {
+        let mut graph = FlowGraph::new(a + b + 2);
+        let source = a + b;
+        let sink = a + b + 1;
+        for i in 0..a {
+            graph.add_edge(source, i, 1, Default::default());
         }
-        flow += f;
+        for j in 0..b {
+            graph.add_edge(a + j, sink, 1, Default::default());
+        }
+        Self {
+            graph,
+            a,
+            b,
+            source,
+            sink,
+        }
+    }
+    pub fn add_edge(&mut self, u: usize, v: usize, weight: E) {
+        self.graph.add_edge(u, self.a + v, 1, weight);
+    }
+    pub fn max_match<F: MaxFlow<E>>(&self) -> Vec<&Edge<E>> {
+        F::max_flow(&self.graph, self.source, self.sink);
+        let mut ret = vec![];
+        for i in 0..self.a {
+            for e in &self.graph.edges[i] {
+                if e.to < self.source && e.cap.get() == 0 {
+                    ret.push(e);
+                }
+            }
+        }
+        ret
     }
 }
