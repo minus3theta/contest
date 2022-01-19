80713748cb133a771af4ae922e7d6c42c534d704
minus3theta <yone_j_synthesis@yahoo.co.jp>
2022-01-19 22:44:37 +0900

    Add ABC 211 A, B, C, D

---
+++ cargo/abc211/src/bin/b.rs
@@ -0,0 +1,25 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use num::Integer;
+#[allow(unused_imports)]
+use proconio::derive_readable;
+use proconio::fastout;
+use proconio::input;
+#[allow(unused_imports)]
+use proconio::marker::*;
+#[allow(unused_imports)]
+use std::cmp;
+#[allow(unused_imports)]
+use std::collections::*;
+
+#[fastout]
+fn main() {
+    input! {
+        mut s: [String; 4],
+    }
+    s.sort();
+    s.dedup();
+
+    println!("{}", if s.len() == 4 { "Yes" } else { "No" });
+}
