2e95f68e93dbb5e81d1fd19eb3e9e5187812a39d
minus3theta <yone_j_synthesis@yahoo.co.jp>
2022-04-02 21:05:41 +0900

    Add GCJ 2022 Qual A, B, C

---
+++ gcj/gcj2022qual/src/bin/b.rs
@@ -0,0 +1,81 @@
+// Input macro from https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+
+}
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, [ $t:tt ]) => {{
+        let len = read_value!($iter, usize);
+        (0..len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    }};
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    input! {
+        t: usize,
+        qs: [[[i64; 4]; 3]; t],
+    }
+    for (i, v) in qs.iter().enumerate() {
+        print!("Case #{}:", i + 1);
+        let mut mins = vec![1_000_000; 4];
+        for p in v {
+            for (i, &x) in p.iter().enumerate() {
+                mins[i] = mins[i].min(x);
+            }
+        }
+        let mut total = 1_000_000;
+        if mins.iter().sum::<i64>() < total {
+            println!(" IMPOSSIBLE");
+        } else {
+            for &x in &mins {
+                let used = x.min(total);
+                print!(" {}", used);
+                total -= used;
+            }
+            println!();
+        }
+    }
+}
